ca74ae957727d29e3c602a0f4901f5af3515960d
frlinh <ci_linh@outlook.com>
2022-09-16 19:47:11 -0700

    updating index

--- static/js/app.js
+++ static/js/app.js
@@ -125,7 +125,7 @@ document.querySelectorAll('th').forEach(th => th.addEventListener('click', (() =
   const tbody = table.querySelector('tbody');
   Array.from(tbody.querySelectorAll('tr'))
     .sort(comparer(Array.from(th.parentNode.children).indexOf(th), this.asc = !this.asc))
-    .forEach(tr => tbody.appendChild(tr) );
+    .forEach(tr => tbody.appendChild(tr));
 })))
 
 
